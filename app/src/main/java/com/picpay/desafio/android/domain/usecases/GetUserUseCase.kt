35d6c8e89df782f3ce316462a6b90d08840308ee
Joselaine Aparecida <joselaine.aparecida@gmail.com>
2022-08-09 21:53:58 -0300

    feat: refactor in UseCase

--- app/src/main/java/com/picpay/desafio/android/domain/usecases/GetUserUseCase.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/usecases/GetUserUseCase.kt
@@ -1,17 +1,20 @@
 package com.picpay.desafio.android.domain.usecases
 
-import com.picpay.desafio.android.data.repository.UserRepository
-import com.picpay.desafio.android.domain.models.User
+import com.picpay.desafio.android.domain.model.User
+import com.picpay.desafio.android.domain.repository.UserRepository
 import javax.inject.Inject
 
 class GetUserUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
     suspend operator fun invoke(): Result<List<User>> {
-        return try {
-            Result.success(repository.getUsers())
-        } catch (exception: Exception) {
-            Result.failure(exception)
-        }
+        return repository.getUsers().fold(
+            onSuccess = { users ->
+               Result.success(users)
+            },
+            onFailure = { throwble ->
+                Result.failure(throwble)
+            }
+        )
     }
 }

8ca333e8334577e3f5a227c1356b2977fcb2aad9
Joselaine Aparecida <joselaine.aparecida@gmail.com>
2022-08-09 21:45:22 -0300

    feat: move UserEntity and UserResponse to data

--- app/src/main/java/com/picpay/desafio/android/data/api/PicPayApi.kt
+++ app/src/main/java/com/picpay/desafio/android/data/api/PicPayApi.kt
@@ -1,6 +1,6 @@
 package com.picpay.desafio.android.data.api
 
-import com.picpay.desafio.android.network.response.UserResponse
+import com.picpay.desafio.android.data.model.UserResponse
 import retrofit2.http.GET
 
 interface PicPayApi {
